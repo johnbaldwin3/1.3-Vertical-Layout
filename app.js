8b5e06c37b805f3f761364491c52fc4140062d08
John Baldwin III <john.baldwin.jb3@gmail.com>
2017-01-12 01:11:31 -0500

    Completed Most

---
+++ app.js
@@ -0,0 +1,25 @@
+// $(".timelabel").hover(function(){
+//
+// if ($(".timelabel").hasClass("odd")) {
+//   $(".timelabel").css(background-color:#5D94C4);
+// };
+// });
+
+// if
+//   (".timelabel").css(background-color)= #8BBEE7);
+// } else if {
+//   (".timelabel").css(background-color)= #8BBEE7);
+//
+// }
+//
+// })
+
+//
+// if $(".timelabel").hasClass("even") {
+//
+// $(".even").hover(function () {
+//   $(".even").css(background-color:#8BBEE7):
+//
+// });
+//
+// });
